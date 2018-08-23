20de484bd96b243d366ac2060e7fa9ad0bd479d5
leekarho <k-h_lee@hotmail.co.uk>
2018-08-23 17:42:12 +0100

    skinny controllers

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,15 @@
 class Game
 
+attr_reader :player1, :player2
+
+  def initialize(p1 = Player.new, p2 = Player.new)
+    # don't need defaults as params always feed names
+    @player1 = p1
+    @player2 = p2
+  end
+
   def attack(enemy)
     enemy.take_damage
   end
-  
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,12 +2,18 @@ describe Game do
 
   let(:samir) { double :samir }
   let(:karho) { double :karho }
-  subject(:game) { Game.new }
+  subject(:game) { Game.new(samir, karho) }
 
   describe '#attack' do
     it "damages the opponent" do
       expect(samir).to receive(:take_damage)
       game.attack(samir)
+    end
+  end
+
+  describe '#initalizes' do
+    it 'knows its players' do
+      expect(Game).to respond_to(:new).with(2).arguments
     end
   end
 end
